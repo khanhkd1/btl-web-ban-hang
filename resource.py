480c7a83cdf8e5120025e876dbe601a8bcb464f0
Kiều Duy Khánh <khnhkd@Kieus-MacBook-Pro.local>
2021-05-17 06:13:56 +0700

    add flask admin

--- resource.py
+++ resource.py
@@ -2,9 +2,16 @@ from flask import Flask
 from flask_restful import Api
 from modules.test.Test import Test
 from modules.camera.camera import CameraWithoutId
+from libraries.connect_database import connect_database, Camera
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
+session = connect_database()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'khanhkd'
+admin = Admin(app)
+admin.add_view(ModelView(Camera, session()))
 api = Api(app)
 
 # Resource dashboard
