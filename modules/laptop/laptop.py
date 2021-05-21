74cfe6237391fdc40ce194a8e172bb57606e510a
Kiều Duy Khánh <khnhkd@khnhkd-mac.local>
2021-05-21 21:24:44 +0700

    add api get brand

--- modules/laptop/laptop.py
+++ modules/laptop/laptop.py
@@ -2,7 +2,7 @@
 from libraries.connect_database import connect_database, Laptop, LaptopBrand
 from libraries.libraries import get_default, standardized_data
 from flask_restful import Resource
-from flask import request
+from flask import request, jsonify, make_response
 from sqlalchemy import or_, exc
 
 session = connect_database()
@@ -10,12 +10,12 @@ session = connect_database()
 
 class LaptopWithoutId(Resource):
     def get(self):
+        session_tmp = session()
+        parameters = request.args
+        limit, page, offset, order = get_default(
+            parameters, Laptop.__table__.columns, Laptop
+        )
         try:
-            session_tmp = session()
-            parameters = request.args
-            limit, page, offset, order = get_default(
-                parameters, Laptop.__table__.columns, Laptop
-            )
             query = session_tmp.query(Laptop)
             if "search" in parameters and parameters['search'] != "":
                 search_values = parameters['search'].split(",")
@@ -27,9 +27,53 @@ class LaptopWithoutId(Resource):
                 records[i] = standardized_data(records[i])
                 records[i]['images'] = records[i]['images'].split(',')
                 records[i]['brand'] = session_tmp.query(LaptopBrand).filter_by(id=records[i]['brand']).first().brand
-            return records
+            return make_response(
+                jsonify(
+                    {
+                        "message": "done",
+                        "data": records
+                    }
+                ), 200
+            )
         except exc as e:
             session_tmp.rollback()
-            return []
+            return make_response(
+                jsonify(
+                    {
+                        "message": "fail",
+                        "data": []
+                    }
+                ), 500
+            )
+        finally:
+            session_tmp.close()
+
+
+class LaptopBrandWithoutId(Resource):
+    def get(self):
+        session_tmp = session()
+        try:
+            query = session_tmp.query(LaptopBrand)
+            records = query.all()
+            for i in range(len(records)):
+                records[i] = standardized_data(records[i])
+            return make_response(
+                jsonify(
+                    {
+                        "message": "done",
+                        "data": records
+                    }
+                ), 200
+            )
+        except exc as e:
+            session_tmp.rollback()
+            return make_response(
+                jsonify(
+                    {
+                        "message": "fail",
+                        "data": []
+                    }
+                ), 500
+            )
         finally:
             session_tmp.close()
--- resource.py
+++ resource.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
-from modules.camera.camera import CameraWithoutId
-from modules.laptop.laptop import LaptopWithoutId
+from modules.camera.camera import CameraWithoutId, CameraBrandWithoutId
+from modules.laptop.laptop import LaptopWithoutId, LaptopBrandWithoutId
 from modules.user.user import UserApi
 from libraries.connect_database import connect_database, Camera, User, Laptop
 from flask_admin import Admin
@@ -20,7 +20,11 @@ admin.add_view(ModelView(User, session()))
 
 api = Api(app)
 api.add_resource(CameraWithoutId, '/camera', methods=['GET'])
+api.add_resource(CameraBrandWithoutId, '/camera_brand', methods=['GET'])
+
 api.add_resource(LaptopWithoutId, '/laptop', methods=['GET'])
+api.add_resource(LaptopBrandWithoutId, '/laptop_brand', methods=['GET'])
+
 api.add_resource(UserApi, '/user', methods=['POST'])
 
 if __name__ == '__main__':
